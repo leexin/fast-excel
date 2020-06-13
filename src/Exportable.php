b5456d9dcfdbf589b10e899a2c72f78f6108b74e
leexin <xinlee6@gmail.com>
2020-06-13 08:30:57 +0800

    Update exportable to support numeric columns

--- src/Exportable.php
+++ src/Exportable.php
@@ -219,9 +219,7 @@ trait Exportable
     private function transformRow($data)
     {
         return collect($data)->map(function ($value) {
-            return is_int($value) || is_float($value) || is_null($value) ? (string) $value : $value;
-        })->filter(function ($value) {
-            return is_string($value);
+            return is_int($value) || is_float($value) ? ($value*1) : (string) $value;
         });
     }
 
